8bf75c8d1d46bc2d094aa89c2cb3a2630cd65948
Lumos <503267850@qq.com>
2023-09-13 17:50:56 +0800

    update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", six);
+    if let Some(number) = six {
+        println!("Test {}", number);
+    }
+    println!("{:?}", six);
+    println!("{:?}", none);
 }
+
